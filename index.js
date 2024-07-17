6fecaebc09ab82954d787644aa23a8e05cf4fae9
cur10sDEV <specsx@protonmail.com>
2024-07-17 22:19:45 +0530

    feat: add drone shot of car moving
    moving the road so to give the illusion as if a drone is filming from top

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ canvas.width = 300;
 const ctx = canvas.getContext("2d");
 
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 500, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -12,8 +12,15 @@ function animate() {
   car.update();
   // setting height here will make sure the canvas remains full size even upon window change
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  // moving the road so to give the illusion as if a drone is filming from top
+  ctx.translate(0, -car.y + canvas.height * 0.7); // placing the car at safe distance to see the traffic ahead
+
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   // updates the scene based on refresh rate of the screen
   requestAnimationFrame(animate);
 }
