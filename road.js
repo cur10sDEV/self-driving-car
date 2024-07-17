b6c130b5ae4d20b647ac0d211a398035d09d5a66
cur10sDEV <specsx@protonmail.com>
2024-07-17 22:09:43 +0530

    feat: add road borders, refactor: separate logic for lanes and borders

--- road.js
+++ road.js
@@ -10,28 +10,42 @@ class Road {
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    // defining borders for collision detection
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
 
   draw(ctx) {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for (let i = 0; i <= this.laneCount; i++) {
+    // adding dash lines to lanes
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
 
-      // add dashes to middle lines
-      if (i > 0 && i < this.laneCount) {
-        // lane dash lines width of Kpx and then Kpx gap (some value K)
-        ctx.setLineDash([40, 40]);
-      } else {
-        ctx.setLineDash([]);
-      }
-
+      // lane dash lines width of Kpx and then Kpx gap (some value K)
+      ctx.setLineDash([40, 40]);
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+    ctx.setLineDash([]);
+
+    // draw borders
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 
   // upon changing the lane count to any even number(in this case) car is off-center
