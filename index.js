13c96fc357321ef8ff87ab7b7a4edfad2a1c6dd3
cur10sDEV <specsx@protonmail.com>
2024-07-24 22:12:33 +0530

    refactor: rename myCanvas/canvas to carCanvas

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 300;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 300;
 
-const ctx = canvas.getContext("2d");
+const carCtx = carCanvas.getContext("2d");
 
-const road = new Road(canvas.width / 2, canvas.width * 0.9, 5);
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 5);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, true, true, 3.5);
 
 // add traffic - multiple cars
@@ -33,21 +33,21 @@ function animate() {
     traffic[i].update(road.borders, []);
   }
   car.update(road.borders, traffic); // passing the traffic so that car can interact with the traffic
-  // setting height here will make sure the canvas remains full size even upon window change
-  canvas.height = window.innerHeight;
+  // setting height here will make sure the carCanvas remains full size even upon window change
+  carCanvas.height = window.innerHeight;
 
-  ctx.save();
+  carCtx.save();
   // moving the road so to give the illusion as if a drone is filming from top
-  ctx.translate(0, -car.y + canvas.height * 0.7); // placing the car at safe distance to see the traffic ahead
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7); // placing the car at safe distance to see the traffic ahead
 
-  road.draw(ctx);
+  road.draw(carCtx);
   // draw traffic
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx, "red");
+    traffic[i].draw(carCtx, "red");
   }
-  car.draw(ctx, "blue");
+  car.draw(carCtx, "blue");
 
-  ctx.restore();
+  carCtx.restore();
   // updates the scene based on refresh rate of the screen
   requestAnimationFrame(animate);
 }
