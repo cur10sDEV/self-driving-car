410eb006e28a9773f1ff4c0709164ecbe73f5d4c
cur10sDEV <specsx@protonmail.com>
2024-07-26 19:59:49 +0530

    feat: add neural network visualizer

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 300;
-
 const carCtx = carCanvas.getContext("2d");
 
-const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 5);
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 500;
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9, 3);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, true, true, 3.5);
 
 // add traffic - multiple cars
@@ -35,6 +38,7 @@ function animate() {
   car.update(road.borders, traffic); // passing the traffic so that car can interact with the traffic
   // setting height here will make sure the carCanvas remains full size even upon window change
   carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
 
   carCtx.save();
   // moving the road so to give the illusion as if a drone is filming from top
@@ -48,6 +52,9 @@ function animate() {
   car.draw(carCtx, "blue");
 
   carCtx.restore();
+
+  // visualize the NN
+  Visualizer.drawNetwork(networkCtx, car.brain);
   // updates the scene based on refresh rate of the screen
   requestAnimationFrame(animate);
 }
