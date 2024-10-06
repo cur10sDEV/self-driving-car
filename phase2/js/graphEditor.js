0495e00e760037d0cb7a6a1217ebf5ee1f9320ea
cur10sDEV <specsx@protonmail.com>
2024-10-06 09:17:45 +0530

    add: select points on the graph

--- phase2/js/graphEditor.js
+++ phase2/js/graphEditor.js
@@ -5,17 +5,41 @@ class GraphEditor {
 
     this.ctx = this.canvas.getContext("2d");
 
+    this.selected = null;
+    this.hovered = null;
+
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
       const newPoint = new Point(evt.offsetX, evt.offsetY);
+      // check if new point is on top of another point
+      if (this.hovered) {
+        // existing point will be selected
+        this.selected = this.hovered;
+        return;
+      }
+
+      // new point will be added to position of mouse pointer
       this.graph.addPoint(newPoint);
+      // the new point will automatically be selected
+      this.selected = newPoint;
+    });
+
+    this.canvas.addEventListener("mousemove", (evt) => {
+      const newPoint = new Point(evt.offsetX, evt.offsetY);
+      this.hovered = getNearestPoint(newPoint, this.graph.points, 18);
     });
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true });
+    }
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true });
+    }
   }
 }
--- phase2/js/primitives/point.js
+++ phase2/js/primitives/point.js
@@ -5,13 +5,33 @@ class Point {
     this.y = y;
   }
 
-  draw(ctx, size = 18, color = "black") {
+  draw(
+    ctx,
+    { size = 18, color = "black", outline = false, fill = false } = {}
+  ) {
     const rad = size / 2; // calculating radius for circular points
     ctx.beginPath();
     ctx.fillStyle = color;
     // draw a circle at x and y
     ctx.arc(this.x, this.y, rad, 0, Math.PI * 2); // 2pi = 360 deg i.e. full circle
     ctx.fill();
+
+    // draw outline
+    if (outline) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+      ctx.stroke();
+    }
+
+    // draw fill
+    if (fill) {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+      ctx.fillStyle = "yellow";
+      ctx.fill();
+    }
   }
 
   equals(point) {
