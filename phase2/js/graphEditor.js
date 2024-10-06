dddb30cb6057ffd2c881a5a5c9fe25bc4d99450a
cur10sDEV <specsx@protonmail.com>
2024-10-06 09:42:49 +0530

    add: delete point on right click

--- phase2/js/graphEditor.js
+++ phase2/js/graphEditor.js
@@ -13,24 +13,37 @@ class GraphEditor {
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", (evt) => {
-      const newPoint = new Point(evt.offsetX, evt.offsetY);
-      // check if new point is on top of another point
-      if (this.hovered) {
-        // existing point will be selected
-        this.selected = this.hovered;
-        return;
+      // if it is a right click delete the hovered point
+      if (evt.button === 2) {
+        if (this.hovered) {
+          this.graph.removePoint(this.hovered);
+        }
       }
 
-      // new point will be added to position of mouse pointer
-      this.graph.addPoint(newPoint);
-      // the new point will automatically be selected
-      this.selected = newPoint;
+      // if it is a left click
+      if (evt.button === 0) {
+        const newPoint = new Point(evt.offsetX, evt.offsetY);
+        // check if new point is on top of another point
+        if (this.hovered) {
+          // existing point will be selected
+          this.selected = this.hovered;
+          return;
+        }
+
+        // new point will be added to position of mouse pointer
+        this.graph.addPoint(newPoint);
+        // the new point will automatically be selected
+        this.selected = newPoint;
+      }
     });
 
     this.canvas.addEventListener("mousemove", (evt) => {
       const newPoint = new Point(evt.offsetX, evt.offsetY);
       this.hovered = getNearestPoint(newPoint, this.graph.points, 18);
     });
+
+    // disable context menu on right click
+    this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
   }
 
   display() {
