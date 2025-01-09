2e6b01735a1d9e40f48aaf479cbad52814c2d6b6
cur10sDEV <specsx@protonmail.com>
2025-01-10 00:22:48 +0530

    feat: add procedural generation of polygons

--- phase2/js/world.js
+++ phase2/js/world.js
@@ -1,11 +1,23 @@
 class World {
-  constructor(graph, roadWidth = 100, roadRoundness = 10) {
+  constructor(
+    graph,
+    roadWidth = 100,
+    roadRoundness = 10,
+    buildingWidth = 150,
+    buildingMinLength = 150,
+    spacing = 50
+  ) {
     this.graph = graph;
     this.roadWidth = roadWidth;
     this.roadRoundness = roadRoundness;
 
+    this.buildingWidth = buildingWidth;
+    this.buildingMinLength = buildingMinLength;
+    this.spacing = spacing;
+
     this.envelopes = [];
     this.roadBorders = [];
+    this.buildings = [];
 
     this.generate();
   }
@@ -21,6 +33,75 @@ class World {
 
     // finding union between segments
     this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
+    this.buildings = this.#generateBuildings();
+  }
+
+  #generateBuildings() {
+    const tmpEnvelopes = [];
+    for (const seg of this.graph.segments) {
+      tmpEnvelopes.push(
+        new Envelope(
+          seg,
+          this.roadWidth + this.buildingWidth + this.spacing * 2,
+          this.roadRoundness
+        )
+      );
+    }
+
+    // remove inner sections and intersections using union
+    const guides = Polygon.union(tmpEnvelopes.map((e) => e.poly));
+
+    // filter out smaller areas that cant support buildings
+    for (let i = 0; i < guides.length; i++) {
+      const seg = guides[i];
+
+      if (seg.length() < this.buildingMinLength) {
+        guides.splice(i, 1);
+        i--;
+      }
+    }
+
+    const supports = [];
+
+    for (let seg of guides) {
+      const len = seg.length() + this.spacing;
+      const buildingCount = Math.floor(
+        len / (this.buildingMinLength + this.spacing)
+      );
+
+      // spilt the large areas for accomodating multiple buildings along the way
+      const buildingLength = len / buildingCount - this.spacing;
+
+      const dir = seg.directionVector();
+
+      let q1 = seg.p1;
+      let q2 = add(q1, scale(dir, buildingLength));
+      supports.push(new Segment(q1, q2));
+
+      for (let i = 2; i <= buildingCount; i++) {
+        q1 = add(q2, scale(dir, this.spacing));
+        q2 = add(q1, scale(dir, buildingLength));
+        supports.push(new Segment(q1, q2));
+      }
+    }
+
+    // bases for the buildings
+    const bases = [];
+    for (const seg of supports) {
+      bases.push(new Envelope(seg, this.buildingWidth).poly);
+    }
+
+    // remove intersecting bases
+    for (let i = 0; i < bases.length - 1; i++) {
+      for (let j = i + 1; j < bases.length; j++) {
+        if (bases[i].intersectsPoly(bases[j])) {
+          bases.splice(j, 1);
+          j--; // because theres a new element at jth index recheck it
+        }
+      }
+    }
+
+    return bases;
   }
 
   draw(ctx) {
@@ -32,6 +113,9 @@ class World {
     }
     for (const seg of this.roadBorders) {
       seg.draw(ctx, { color: "white", width: 4 });
+    }
+    for (const building of this.buildings) {
+      building.draw(ctx);
     }
   }
 }
